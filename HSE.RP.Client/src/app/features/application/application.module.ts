4bb3984e139bd2c1d1f6f4e2b03de1e114ed574c
Sean Lynch <slynch@codec.ie>
2023-06-13 10:51:37 +0100

    change component / module structure

--- HSE.RP.Client/src/app/features/application/application.module.ts
+++ HSE.RP.Client/src/app/features/application/application.module.ts
@@ -1,38 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HseRoute, HseRoutes } from '../../helpers/hse.route';
-import { ApplicationNameComponent } from './application-name/application-name.component';
-import { ApplicationInfoComponent } from './application-info/application-info.component';
-import { ApplicantPhoneComponent } from './applicant-phone/applicant-phone.component';
-import { ApplicantNameComponent } from './applicant-name/applicant-name.component';
-import { ApplicantEmailComponent } from './applicant-email/applicant-email.component';
 import { RouterModule } from '@angular/router';
-
+import { HseAngularModule } from "hse-angular";
 import { FormsModule } from '@angular/forms';
 import { ComponentsModule } from '../../components/components.module';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ApplicationService } from '../../services/application.service';
-import { HseAngularModule } from 'hse-angular';
 import { CookiesBannerService } from '../../services/cookies-banner.service';
 import { ApplicationTaskListComponent } from './task-list/task-list.component';
+import { ApplicationPersonalDetailsModule } from './1-personal-details/application.personal-details.module';
 
 const routes = new HseRoutes([
-  HseRoute.protected(ApplicationNameComponent.route, ApplicationNameComponent, ApplicationNameComponent.title),
-  HseRoute.unsafe(ApplicationInfoComponent.route, ApplicationInfoComponent, undefined, ApplicationInfoComponent.title),
-  HseRoute.protected(ApplicantPhoneComponent.route, ApplicantPhoneComponent, ApplicantPhoneComponent.title),
-  HseRoute.protected(ApplicantNameComponent.route, ApplicantNameComponent, ApplicantNameComponent.title),
-  HseRoute.protected(ApplicantEmailComponent.route, ApplicantEmailComponent, ApplicantEmailComponent.title),
+  HseRoute.forLoadChildren(ApplicationPersonalDetailsModule.baseRoute, () => import('./1-personal-details/application.personal-details.module').then(m => m.ApplicationPersonalDetailsModule)),
   HseRoute.protected(ApplicationTaskListComponent.route, ApplicationTaskListComponent, ApplicationTaskListComponent.title),
 
 ]);
 
 @NgModule({
   declarations: [
-    ApplicationInfoComponent,
-    ApplicantPhoneComponent,
-    ApplicantNameComponent,
-    ApplicantEmailComponent,
-    ApplicationNameComponent,
     ApplicationTaskListComponent,
 
   ],
