a75860af0ca3729157bbbbca2df247e8972fb288
Sean Lynch <slynch@codec.ie>
2023-06-13 12:30:38 +0100

    set style in component for link in ul

--- HSE.RP.Client/src/app/features/home/home.component.ts
+++ HSE.RP.Client/src/app/features/home/home.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  styles: ['a:link.info { color: black; }',
+           'a:visited.info { color: black; }',
+           'a:hover.info { text-decoration-thickness: max(3px, .1875rem, .12em); -webkit-text-decoration-skip-ink: none; text-decoration-skip-ink: none; -webkit-text-decoration-skip: none; }'],
 })
 export class HomeComponent {
   public static route: string = "";
