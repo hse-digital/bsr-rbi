737ffbd2ed1d0417e2d2be5b8d44b140cb0d707f
Jonathan Lydiate <Jonathan.Lydiate@hse.gov.uk>
2023-07-19 07:23:47 +0100

    Connecting model to view

--- HSE.RP.Client/src/app/features/application/2-building-inspector-class/class-selection/building-inspector-class-selection.component.ts
+++ HSE.RP.Client/src/app/features/application/2-building-inspector-class/class-selection/building-inspector-class-selection.component.ts
@@ -20,7 +20,7 @@ export class BuildingInspectorClassSelectionComponent extends PageComponent<stri
   modelValid: boolean = false;
   photoHasErrors = false;
   override model?: string;
-  selectedOption?: string = "";
+  selectedOption: string = "";
   selectedOptionError: boolean = false;
   errorMessage: string = "";
 
@@ -30,10 +30,8 @@ export class BuildingInspectorClassSelectionComponent extends PageComponent<stri
   }
 
   override onInit(applicationService: ApplicationService): void {
-    //this.model = applicationService.model.personalDetails?.applicantPhoto?.toString() ?? '';
-    console.log(applicationService.model)
     console.log(applicationService)
-    // this.model = applicationService.model.BuildingInspectorClass.ClassSelection; 
+    this.model = applicationService.model.BuildingInspectorClass?.ClassSelection; 
     
     if (this.model === "1") {
       this.selectedOption = "1"
@@ -47,9 +45,7 @@ export class BuildingInspectorClassSelectionComponent extends PageComponent<stri
   }
 
   override async onSave(applicationService: ApplicationService): Promise<void> {
-    this.model = applicationService.model.BuildingInspectorClass!.ClassSelection;
-    console.log(this.model)
-
+    this.applicationService.model.BuildingInspectorClass!.ClassSelection = this.model;
     // applicationService.model.ApplicationStatus = ApplicationStatus.BuildingInspectorClassComplete;
   }
 
@@ -66,23 +62,26 @@ export class BuildingInspectorClassSelectionComponent extends PageComponent<stri
       return false;
     }
 
+    this.model = this.selectedOption;
     return true;
   }
 
   override navigateNext(): Promise<boolean> {
     console.log(this.model)
-    // if (this.selectedOption === "1") {
+    if (this.selectedOption === "1") {
       return this.navigationService.navigateRelative(
         BuildingInspectorCountryComponent.route,
         this.activatedRoute
       );
-    //}
+    }
 
-    //TODO the next page is missing so will have to create a shell for it then do navigation
-    // if (this.selectedOption === "2" || "3"){
-    //   return this.navigationService.navigateRelative(
-    //     BuildingInspect
-    //   )
-    // }
+    // if (this.selectedOption === "2" || "3") {
+    else {
+      return this.navigationService.navigateRelative(
+        //TODO the next page is missing so will have to create a shell for it then do navigation
+        BuildingInspectorCountryComponent.route,
+        this.activatedRoute
+      )
+    }
   }
 }
