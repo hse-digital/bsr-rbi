e013cdfafeb50462a2b968d26152dd5cc7f4f572
Jonathan Lydiate <Jonathan.Lydiate@hse.gov.uk>
2023-07-19 07:22:45 +0100

    Updating Azure Function record model

--- HSE.RP.API/Models/BuildingProfessionApplicationModel.cs
+++ HSE.RP.API/Models/BuildingProfessionApplicationModel.cs
@@ -8,13 +8,11 @@ using System.Threading.Tasks;
 
 namespace HSE.RP.API.Models
 {
-    //! going to have to add building class record model here (same as personal details)
-
     public record BuildingProfessionApplicationModel
     (
         [property: JsonPropertyName("id")] string Id,
         PersonalDetails PersonalDetails = null,
-        //BuildingInspectorClass BuildingInspectorClass = null,
+        BuildingInspectorClass BuildingInspectorClass = null,
         ApplicationStatus ApplicationStatus = ApplicationStatus.None) : IValidatableModel
     {
         public ValidationSummary Validate()
@@ -79,13 +77,12 @@ namespace HSE.RP.API.Models
         public string ClassificationCode { get; init; }
     }
 
-    //! going to have to add building class record model here
-    //public record BuildingInspectorClass
-    //(
-    //    string ClassSelection = null,
-    //    string ClassDetails = null,
-    //    string Country = null
-    //);
+    public record BuildingInspectorClass
+    (
+        string ClassSelection = null,
+        string ClassDetails = null,
+        string Country = null
+    );
 
     [Flags]
     public enum ApplicationStatus
