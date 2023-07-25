f22a82744fa588f7ff1c3dd56e985d7eec664166
Jonathan Lydiate <Jonathan.Lydiate@hse.gov.uk>
2023-07-25 10:49:34 +0100

    Class2/3 summary remade with plain html to support two change links in one row

--- HSE.RP.Client/src/app/features/application/2-building-inspector-class/building-inspector-summary/building-inspector-summary.component.ts
+++ HSE.RP.Client/src/app/features/application/2-building-inspector-class/building-inspector-summary/building-inspector-summary.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
 import { environment } from '../../../../../environments/environment';
 import { PageComponent } from '../../../../helpers/page.component';
 import { FieldValidations } from '../../../../helpers/validators/fieldvalidations';
-import { ApplicationService, ApplicationStatus } from '../../../../services/application.service';
+import { ApplicationService, ApplicationStatus, BuildingInspectorClassType } from '../../../../services/application.service';
 import { takeLast } from 'rxjs';
 import { ApplicationTaskListComponent } from '../../task-list/task-list.component';
 import { BuildingInspectorRoutes, BuildingInspectorRouter } from '../BuildingInspectorRoutes';
@@ -18,6 +18,7 @@ export class BuildingInspectorSummaryComponent extends PageComponent<string> {
   }
 
   BuildingInspectorRoutes = BuildingInspectorRoutes;
+  BuildingInspectorClassType = BuildingInspectorClassType;
 
   public static route: string = BuildingInspectorRoutes.SUMMARY;
   static title: string = "Building inspector class - Register as a building inspector - GOV.UK";
@@ -27,7 +28,10 @@ export class BuildingInspectorSummaryComponent extends PageComponent<string> {
   override model?: string;
 
   assessPlansCategories: string = "";
+  assessPlansLink: string = "";
+
   inspectionCategories: string = "";
+  inspectionLink: string = "";
 
   constructor(activatedRoute: ActivatedRoute, applicationService: ApplicationService) {
     super(activatedRoute);
@@ -36,11 +40,23 @@ export class BuildingInspectorSummaryComponent extends PageComponent<string> {
 
   override onInit(applicationService: ApplicationService): void {
     //this.model = applicationService.model.personalDetails?.applicantPhoto?.toString() ?? '';
-    console.log(applicationService.model)
 
-    //? can do this with pipes
-    this.inspectionCategories = this.categoriesToString(applicationService.model.InspectorClass?.BuildingPlanCategories!)
-    this.assessPlansCategories = this.categoriesToString(applicationService.model.InspectorClass?.AssessingPlansClass3)
+    if (applicationService.model.InspectorClass?.ClassType.Class === BuildingInspectorClassType.Class2)
+    {
+      this.assessPlansLink = BuildingInspectorRoutes.CLASS2_ACCESSING_PLANS_CATEGORIES;
+      this.inspectionLink = BuildingInspectorRoutes.CLASS2_INSPECT_BUILDING_CATEGORIES;
+      this.inspectionCategories = this.categoriesToString(applicationService.model.InspectorClass?.Class2InspectBuildingCategories)
+      this.assessPlansCategories = this.categoriesToString(applicationService.model.InspectorClass?.BuildingAssessingPlansCategoriesClass2)
+    }
+
+    if (applicationService.model.InspectorClass?.ClassType.Class === BuildingInspectorClassType.Class3)
+    {
+      this.assessPlansLink = BuildingInspectorRoutes.CLASS3_ACCESSING_PLANS_CATEGORIES;
+      this.inspectionLink = BuildingInspectorRoutes.CLASS3_INSPECT_BUILDING_CATEGORIES;
+      
+      this.inspectionCategories = this.categoriesToString(applicationService.model.InspectorClass?.Class3InspectBuildingCategories)
+      this.assessPlansCategories = this.categoriesToString(applicationService.model.InspectorClass?.BuildingAssessingPlansCategoriesClass3)
+    }
   }
 
   override async onSave(applicationService: ApplicationService): Promise<void> {
@@ -71,7 +87,7 @@ export class BuildingInspectorSummaryComponent extends PageComponent<string> {
   categoriesToString(categories: any): string {
     // loop through keys in object
     // if keys value is true, add the last character of the key name to string with a comma
-    // when the loop is complete, remove the trailing comma
+    // when the loop is complete, remove the trailing comma and space
     let newString = "";
 
     Object.keys(categories).forEach(key => {
