8e0a3ff19a5be2cf962b17228fa50a9a39cb4628
Sean Lynch <slynch@codec.ie>
2023-08-04 12:06:07 +0100

    add entity #9663

---
+++ HSE.RP.Domain/Entities/AssessmentOrganisationNames.cs
@@ -0,0 +1,14 @@
+﻿namespace HSE.RP.Domain.Entities;
+
+public static class AssessmentOrganisationNames
+{
+    public static readonly IDictionary<string, string> Ids = new Dictionary<string, string>
+    {
+        ["BSCF"] = "4fb54d8a-262a-ee11-9965-0022481b59de",
+        ["CABE"] = "f36d4890-262a-ee11-9965-0022481b59de",
+        ["RICS"] = "48f60522-932c-ee11-9965-0022481b59de",
+        ["CIOB"] = "689d678f-962c-ee11-9965-0022481b59de",
+        ["Other"] = "65f5b095-962c-ee11-9965-0022481b59de",
+
+    };
+}
