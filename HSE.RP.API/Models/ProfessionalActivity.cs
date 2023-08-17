f246e183985053409ed104f77b6102490b0bc5e3
Sean Lynch <slynch@codec.ie>
2023-08-17 21:44:56 +0100

    [WIP]

--- HSE.RP.API/Models/ProfessionalActivity.cs
+++ HSE.RP.API/Models/ProfessionalActivity.cs
@@ -5,6 +5,6 @@ namespace HSE.RP.API.Models
     public record ProfessionalActivity
     {
         public ApplicantProfessionalBodyMembership ApplicantProfessionalBodyMembership { get; set; }
-        public EmploymentTypeSelection EmploymentTypeSelection { get; set; }
+        public ApplicantEmploymentDetails EmploymentDetails { get; set; }
     }
 }
