26511813dd7a55998bfc8ca11d53ad54151b3256
Jonathan Lydiate <Jonathan.Lydiate@hse.gov.uk>
2023-08-22 13:04:03 +0100

    Adding prefix match back in

--- HSE.RP.Client/src/app/features/application/3-competency/assessment-certificate-number/competency-assessment-certificate-number.component.ts
+++ HSE.RP.Client/src/app/features/application/3-competency/assessment-certificate-number/competency-assessment-certificate-number.component.ts
@@ -44,8 +44,6 @@ export class CompetencyAssessmentCertificateNumberComponent extends PageComponen
 
     this.organisationPrefix = applicationService.model.Competency!.CompetencyAssessmentOrganisation!.ComAssessmentOrganisation;
     applicationService.model.Competency!.CompetencyAssessmentCertificateNumber!.CompletionState = ComponentCompletionState.InProgress;
-  
-    console.log(this.organisationPrefix)
   }
 
   override async onSave(applicationService: ApplicationService): Promise<void> {
@@ -79,6 +77,15 @@ export class CompetencyAssessmentCertificateNumberComponent extends PageComponen
     if (!validCertificateNumRegex) {
       this.errorMessage = "You must enter an assessment certificate number in the correct format";
       this.hasErrors = true;
+      return false;
+    }
+
+    // 4 character prefix (CABE or BSCF) with a 20 character max string so CABE1262IJSBFAHS840.
+    let prefix = this.certificateNumber.slice(0, 4);
+    if (prefix.toLowerCase() !== this.organisationPrefix.toLowerCase()) {
+      this.errorMessage = "You must enter an assessment certificate number in the correct format";
+      this.hasErrors = true;
+
       return false;
     }
 
