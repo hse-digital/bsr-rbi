6648bc9e324b62edd4ce1b31efe04066b0d303c1
KUsman <Kusman.codecsystems@hsegov.onmicrosoft.com>
2023-08-24 15:33:33 +0000

    Merged PR 820: fixed bugs for membership and employement section
    fixed bugs for membership and employement section
    
    Related work items: #12878, #12886, #12889

--- HSE.RP.Client/src/app/features/application/4-professional-activity/employment-type/professional-activity-employment-type.component.ts
+++ HSE.RP.Client/src/app/features/application/4-professional-activity/employment-type/professional-activity-employment-type.component.ts
@@ -18,59 +18,125 @@ import { GovukTaskListComponent } from 'hse-angular';
 import { EmploymentPrivateSectorBodyNameComponent } from '../employment-private-sector-body-name/employment-private-sector-body-name.component';
 import { ProfessionalMembershipAndEmploymentSummaryComponent } from '../professional-membership-and-employment-summary/professional-membership-and-employment-summary.component';
 
-
 @Component({
   selector: 'hse-professional-activity-employment-type',
   templateUrl: './professional-activity-employment-type.component.html',
 })
 export class ProfessionalActivityEmploymentTypeComponent extends PageComponent<EmploymentTypeSelection> {
-  DerivedIsComplete(value: boolean): void {
+  DerivedIsComplete(value: boolean): void {}
 
-  }
-
-  public static route: string = "professional-activity-employment-type";
-  static title: string = "Professional activity - Register as a building inspector - GOV.UK";
+  public static route: string = 'professional-activity-employment-type';
+  static title: string =
+    'Professional activity - Register as a building inspector - GOV.UK';
   production: boolean = environment.production;
   modelValid: boolean = false;
   photoHasErrors = false;
   // override model?: EmploymentType;
   selectedOptionError: boolean = false;
-  errorMessage: string = "";
+  errorMessage: string = '';
   EmploymentType = EmploymentType;
+  existingEmploymentType?: EmploymentType =
+    this.applicationService.model.ProfessionalActivity.EmploymentDetails
+      ?.EmploymentTypeSelection?.EmploymentType;
 
-  constructor(activatedRoute: ActivatedRoute, applicationService: ApplicationService) {
+  constructor(
+    activatedRoute: ActivatedRoute,
+    applicationService: ApplicationService
+  ) {
     super(activatedRoute);
     this.updateOnSave = true;
   }
 
   override onInit(applicationService: ApplicationService): void {
-
-    this.model = applicationService.model.ProfessionalActivity.EmploymentDetails?.EmploymentTypeSelection;
+    this.model =
+      applicationService.model.ProfessionalActivity.EmploymentDetails?.EmploymentTypeSelection;
     // if the user visits this page for the first time, set status to in progress until user saves and continues
-    if (applicationService.model.ProfessionalActivity.EmploymentDetails?.EmploymentTypeSelection?.EmploymentType === EmploymentType.None)
-    {
-      applicationService.model.ProfessionalActivity.EmploymentDetails.EmploymentTypeSelection = { EmploymentType: EmploymentType.None, CompletionState: ComponentCompletionState.InProgress};
+    if (
+      applicationService.model.ProfessionalActivity.EmploymentDetails
+        ?.EmploymentTypeSelection?.EmploymentType === EmploymentType.None
+    ) {
+      applicationService.model.ProfessionalActivity.EmploymentDetails.EmploymentTypeSelection =
+        {
+          EmploymentType: EmploymentType.None,
+          CompletionState: ComponentCompletionState.InProgress,
+        };
     }
-    if(applicationService.model.ProfessionalActivity.EmploymentDetails?.CompletionState!=ComponentCompletionState.Complete){
-      applicationService.model.ProfessionalActivity.EmploymentDetails!.CompletionState = ComponentCompletionState.InProgress;
+    if (
+      applicationService.model.ProfessionalActivity.EmploymentDetails
+        ?.CompletionState != ComponentCompletionState.Complete
+    ) {
+      applicationService.model.ProfessionalActivity.EmploymentDetails!.CompletionState =
+        ComponentCompletionState.InProgress;
     }
+  }
+
+  override async saveAndComeBack(): Promise<void> {
+    this.processing = true;
+
+    const status =
+      this.applicationService.model.ProfessionalActivity.EmploymentDetails
+        ?.CompletionState;
+
+    if (
+      this.model?.EmploymentType === this.existingEmploymentType &&
+      status === ComponentCompletionState.Complete
+    ) {
+      this.applicationService.model.ProfessionalActivity.EmploymentDetails!.CompletionState =
+        ComponentCompletionState.Complete;
+    } else {
+      this.applicationService.model.ProfessionalActivity.EmploymentDetails!.CompletionState =
+        ComponentCompletionState.InProgress;
+    }
+
+    this.applicationService.model.ProfessionalActivity.EmploymentDetails!.EmploymentTypeSelection!.EmploymentType =
+      this.model?.EmploymentType;
+
+    if (!this.hasErrors) {
+      this.triggerScreenReaderNotification();
+      this.applicationService.updateLocalStorage();
+      await this.applicationService.updateApplication();
+    } else {
+      this.focusAndUpdateErrors();
+    }
+
+    this.processing = false;
+
+    const taskListRoute: string = `application/${this.applicationService.model.id}`;
+    this.navigationService.navigate(taskListRoute);
   }
 
   override async onSave(applicationService: ApplicationService): Promise<void> {
-    applicationService.model.ProfessionalActivity.EmploymentDetails!.EmploymentTypeSelection = this.model;
+    applicationService.model.ProfessionalActivity.EmploymentDetails!.EmploymentTypeSelection =
+      this.model;
+
+    const status =
+      this.applicationService.model.ProfessionalActivity.EmploymentDetails
+        ?.CompletionState;
+
+    if (
+      this.model?.EmploymentType === this.existingEmploymentType &&
+      status === ComponentCompletionState.Complete
+    ) {
+      this.applicationService.model.ProfessionalActivity.EmploymentDetails!.CompletionState =
+        ComponentCompletionState.Complete;
+    } else {
+      this.applicationService.model.ProfessionalActivity.EmploymentDetails!.CompletionState =
+        ComponentCompletionState.InProgress;
+    }
   }
 
-  override canAccess(applicationService: ApplicationService, routeSnapshot: ActivatedRouteSnapshot): boolean {
+  override canAccess(
+    applicationService: ApplicationService,
+    routeSnapshot: ActivatedRouteSnapshot
+  ): boolean {
     return true;
     //return (FieldValidations.IsNotNullOrWhitespace(applicationService.model?.personalDetails?.applicatantName?.firstName) || FieldValidations.IsNotNullOrWhitespace(applicationService.model?.personalDetails?.applicatantName?.lastName));
-
   }
-
 
   override isValid(): boolean {
     if (!this.model?.EmploymentType) {
       this.selectedOptionError = true;
-      this.errorMessage = "Select your current employment status";
+      this.errorMessage = 'Select your current employment status';
       return false;
     }
 
@@ -78,28 +144,33 @@ export class ProfessionalActivityEmploymentTypeComponent extends PageComponent<E
   }
 
   override navigateNext(): Promise<boolean> {
-
-    if(this.model?.EmploymentType == EmploymentType.Other)
-    {
-      return this.navigationService.navigateRelative(EmploymentOtherNameComponent.route, this.activatedRoute);
+    if (this.model?.EmploymentType == EmploymentType.Other) {
+      return this.navigationService.navigateRelative(
+        EmploymentOtherNameComponent.route,
+        this.activatedRoute
+      );
     }
-    if(this.model?.EmploymentType == EmploymentType.PublicSector)
-    {
-      return this.navigationService.navigateRelative(EmploymentPublicSectorBodyNameComponent.route, this.activatedRoute);
+    if (this.model?.EmploymentType == EmploymentType.PublicSector) {
+      return this.navigationService.navigateRelative(
+        EmploymentPublicSectorBodyNameComponent.route,
+        this.activatedRoute
+      );
     }
-    if(this.model?.EmploymentType == EmploymentType.PrivateSector)
-    {
-      return this.navigationService.navigateRelative(EmploymentPrivateSectorBodyNameComponent.route, this.activatedRoute);
+    if (this.model?.EmploymentType == EmploymentType.PrivateSector) {
+      return this.navigationService.navigateRelative(
+        EmploymentPrivateSectorBodyNameComponent.route,
+        this.activatedRoute
+      );
     }
-    if(this.model?.EmploymentType == EmploymentType.Unemployed)
-    {
-      return this.navigationService.navigateRelative(ProfessionalMembershipAndEmploymentSummaryComponent.route, this.activatedRoute);
+    if (this.model?.EmploymentType == EmploymentType.Unemployed) {
+      return this.navigationService.navigateRelative(
+        ProfessionalMembershipAndEmploymentSummaryComponent.route,
+        this.activatedRoute
+      );
+    } else {
+      return this.navigationService.navigate(
+        `application/${this.applicationService.model.id}`
+      );
     }
-    else{
-      return this.navigationService.navigate(`application/${this.applicationService.model.id}`);
-
-    }
-
   }
-
 }
