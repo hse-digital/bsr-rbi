5815a0de33f34a0cc437a12076681d40f4f4e110
Sean Lynch Codec Systems <slynch@codec.ie>
2023-08-29 08:08:27 +0000

    Merged PR 841: Add dupe application check
    Related work items: #5888

--- HSE.RP.API/Services/DynamicsService.cs
+++ HSE.RP.API/Services/DynamicsService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Reflection.Metadata;
@@ -17,6 +18,7 @@ using HSE.RP.API.Models.LocalAuthority;
 using HSE.RP.API.Models.Payment.Response;
 using HSE.RP.Domain.DynamicsDefinitions;
 using HSE.RP.Domain.Entities;
+using Microsoft.DurableTask;
 using Microsoft.Extensions.Options;
 using Microsoft.Identity.Client;
 using static System.Net.Mime.MediaTypeNames;
@@ -572,11 +574,11 @@ namespace HSE.RP.API.Services
                 //If no entry exists then create a new one
                 if (existingRegistrationActivity == null)
                 {
-                    
+
                     var response = await dynamicsApi.Create("bsr_biemploymentdetails", dynamicsBuildingInspectorEmploymentDetail);
                     var buildingInspectorEmploymentDetailId = ExtractEntityIdFromHeader(response.Headers);
                     return buildingInspectorEmploymentDetail with { Id = buildingInspectorEmploymentDetailId };
-               
+
                 }
                 //If an entry exists then update it
                 else
@@ -679,10 +681,35 @@ namespace HSE.RP.API.Services
             return response.value.FirstOrDefault();
         }
 
+        public async Task<bool> CheckDupelicateBuildingProfessionApplicationAsync(BuildingProfessionApplicationModel buildingProfessionApplicationModel)
+        {
 
+#if DEBUG
+            return false;
+#endif
+            //Check for existing contact
+            var contact = await dynamicsApi.Get<DynamicsResponse<DynamicsContact>>("contacts", new[]
+            {
+                        ("$filter", $"firstname eq '{buildingProfessionApplicationModel.PersonalDetails.ApplicantName.FirstName.EscapeSingleQuote()}' and lastname eq '{buildingProfessionApplicationModel.PersonalDetails.ApplicantName.LastName.EscapeSingleQuote()}' and statuscode eq 1 and emailaddress1 eq '{buildingProfessionApplicationModel.PersonalDetails.ApplicantEmail.Email.EscapeSingleQuote()}'"),
+            });
 
+            //If contact exists check for existing application
+            if (contact.value.FirstOrDefault() != null)
+            {
+                var application = await dynamicsApi.Get<DynamicsResponse<DynamicsBuildingProfessionApplication>>("bsr_buildingprofessionapplications", new[]
+            {
+                        ("$filter", $"_bsr_applicantid_value eq '{contact.value.FirstOrDefault().contactid}' and statuscode ne 1  and statuscode ne 1"),
+            });
+
+                return application.value.Count>0;
+            }
+            else
+            {
+                return false;   
+
+            }
+
+        }
     }
-
-
 }
 
