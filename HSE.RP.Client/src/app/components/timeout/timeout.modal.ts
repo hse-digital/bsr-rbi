c06280ffa0ef7e56b6ed90f7fe661ae8cf5b489e
Sean Lynch <slynch@codec.ie>
2023-09-11 14:37:58 +0100

    update tabindex for timeout modal

--- HSE.RP.Client/src/app/components/timeout/timeout.modal.ts
+++ HSE.RP.Client/src/app/components/timeout/timeout.modal.ts
@@ -13,6 +13,7 @@ export class TimeoutModalComponent implements OnInit {
     @Output() onSaveAndComebackClicked = new EventEmitter();
     @Output() onTimeout = new EventEmitter();
 
+
     interval?: any;
     ngOnInit(): void {
         this._remainingSeconds = this.delayToTimeout;
