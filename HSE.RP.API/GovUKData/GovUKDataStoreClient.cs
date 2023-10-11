946c9cd8a71f3fa0b400d4a3c61484739f774083
Mark Lysaght <mlysaght@codec.ie>
2023-10-11 14:27:50 +0100

    Revert "remove unused file"
    This reverts commit 12bffe23d58a2fce164a854dc619db1eeace47e5.

---
+++ HSE.RP.API/GovUKData/GovUKDataStoreClient.cs
@@ -0,0 +1,27 @@
+﻿// Doesn't look like were using this?
+
+
+
+//using Azure.Storage.Blobs;
+//using Microsoft.Extensions.Options;
+
+//namespace HSE.RP.API.BlobStore;
+
+//public interface IGovUKDataStoreClient
+//{
+//    BlobServiceClient GetGovUKDataClient();
+//}
+//public class GovUKDataStoreClient : IGovUKDataStoreClient
+//{
+//    private readonly GovUKDataOptions _govUKDataOptions;
+
+//    public GovUKDataStoreClient(IOptions<GovUKDataOptions> govUKDataOptions)
+//    {
+//        this._govUKDataOptions = govUKDataOptions.Value;
+//    }
+
+//    public BlobServiceClient GetGovUKDataClient()
+//    {
+//        return new BlobServiceClient(_govUKDataOptions.ConnectionString);
+//    }
+//}
