047bcf9804b97e83fb7ec074924d05820c345ce0
Mark Lysaght Codec Systems <MLysaght.codecsystems@hsegov.onmicrosoft.com>
2023-10-18 11:28:25 +0000

    Merged PR 1434: Build integration tests

---
+++ HSE.RP.API.IntegrationTests/Startup.cs
@@ -0,0 +1,30 @@
+﻿using HSE.RP.API.Models;
+using HSE.RP.API.Services;
+using HSE.RP.Domain.DynamicsDefinitions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace HSE.RP.API.IntegrationTests;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services, HostBuilderContext context)
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        services.Configure<DynamicsOptions>(config.GetSection(DynamicsOptions.Dynamics));
+        services.Configure<IntegrationsOptions>(config.GetSection(IntegrationsOptions.Integrations));
+        services.Configure<SwaOptions>(config.GetSection(SwaOptions.Swa));
+
+        services.AddTransient<DynamicsService>();
+        services.AddTransient<DynamicsApi>();
+        services.AddTransient<OTPService>();
+        services.AddTransient<DynamicsModelDefinitionFactory>();
+    }
+
+}
