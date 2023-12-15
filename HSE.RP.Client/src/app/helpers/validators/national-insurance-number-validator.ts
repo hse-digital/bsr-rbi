4113454347753aae2dfab499d9f26359caa787a4
Sean Lynch <slynch@codec.ie>
2023-12-15 12:34:39 +0000

    Allow loose validation #14826

--- HSE.RP.Client/src/app/helpers/validators/national-insurance-number-validator.ts
+++ HSE.RP.Client/src/app/helpers/validators/national-insurance-number-validator.ts
@@ -1,7 +1,7 @@
 export class NationalInsuranceNumberValidator {
 
   private static _isNationalInsuranceNumberValid(nationalInsuranceNumber: string): boolean {
-    return new RegExp(/^(?!BG)(?!GB)(?!NK)(?!KN)(?!TN)(?!NT)(?!ZZ)[A-Z]*[^DFIQUV][A-Z]*[^DFIOQUV][0-9]{6}[A-D]$/, "gmi").test(nationalInsuranceNumber.replace(/\s/g, ''));
+    return new RegExp(/^[A-Z][A-Z][0-9]{6}[A-Z]$$/, "gmi").test(nationalInsuranceNumber.replace(/\s/g, ''));
   }
 
 
