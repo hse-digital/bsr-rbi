fa174f2421d52e377495877fdfd874f924328da7
Sean Lynch <slynch@codec.ie>
2024-01-19 10:49:55 +0000

    Update duplicate application check to include application type #20998

--- HSE.RP.API/Services/DynamicsService.cs
+++ HSE.RP.API/Services/DynamicsService.cs
@@ -840,7 +840,6 @@ namespace HSE.RP.API.Services
             if (featureOptions.DisableApplicationDuplicationCheck)
             {
                 return false;
-
             }
 
             //Check for existing contact
@@ -854,7 +853,7 @@ namespace HSE.RP.API.Services
             {
                 var application = await dynamicsApi.Get<DynamicsResponse<DynamicsBuildingProfessionApplication>>("bsr_buildingprofessionapplications", new[]
             {
-                        ("$filter", $"_bsr_applicantid_value eq '{contact.value.FirstOrDefault().contactid}' and statecode ne 1"),
+                        ("$filter", $"_bsr_applicantid_value eq '{contact.value.FirstOrDefault().contactid}' and statecode ne 1 and bsr_buildingprofessiontypecode eq {(int)BuildingProfessionType.BuildingInspector}"),
             });
 
                 return application.value.Count > 0;
