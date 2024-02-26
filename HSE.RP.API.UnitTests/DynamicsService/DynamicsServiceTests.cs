cf9a3cd9ec5bb900c6ec60a0d35b1dc136773ffd
Sean Lynch <slynch@codec.ie>
2024-02-26 16:29:09 +0000

    add test for FindExistingContactAsync

--- HSE.RP.API.UnitTests/DynamicsService/DynamicsServiceTests.cs
+++ HSE.RP.API.UnitTests/DynamicsService/DynamicsServiceTests.cs
@@ -476,6 +476,36 @@ namespace HSE.RP.API.UnitTests.DynamicsServiceTest
         }
 
         [Fact]
+        public async Task FindExistingContactAsync()
+        {
+
+            //Arrange
+
+            HttpTest.ForCallsTo($"{DynamicsOptions.EnvironmentUrl}/api/data/v9.2/contacts")
+            .WithAnyQueryParam("$filter", $"firstname eq '{buildingProfessionApplicationModelNewApplication.PersonalDetails.ApplicantName.FirstName.EscapeSingleQuote()}' and lastname eq '{buildingProfessionApplicationModelNewApplication.PersonalDetails.ApplicantName.LastName.EscapeSingleQuote()}' and statuscode eq 1 and emailaddress1 eq '{buildingProfessionApplicationModelNewApplication.PersonalDetails.ApplicantEmail.Email.EscapeSingleQuote()}' and contains(telephone1, '{buildingProfessionApplicationModelNewApplication.PersonalDetails.ApplicantPhone.PhoneNumber.Replace("+", string.Empty).EscapeSingleQuote()}')")
+            .WithQueryParam("$expand", "bsr_contacttype_contact")
+            .WithVerb(HttpMethod.Get)
+            .RespondWithJson(new DynamicsResponse<DynamicsContact> { value = new List<DynamicsContact> { dynamicsContact } });
+
+
+            //Act
+
+            var testGetContacts = await _dynamicsService.FindExistingContactAsync(buildingProfessionApplicationModelNewApplication.PersonalDetails.ApplicantName.FirstName, buildingProfessionApplicationModelNewApplication.PersonalDetails.ApplicantName.LastName, buildingProfessionApplicationModelNewApplication.PersonalDetails.ApplicantEmail.Email,buildingProfessionApplicationModelNewApplication.PersonalDetails.ApplicantPhone.PhoneNumber);
+
+
+
+            //Assert
+            HttpTest.ShouldHaveCalled($"{DynamicsOptions.EnvironmentUrl}/api/data/v9.2/contacts")
+            .WithAnyQueryParam("$filter", $"firstname eq '{buildingProfessionApplicationModelNewApplication.PersonalDetails.ApplicantName.FirstName.EscapeSingleQuote()}' and lastname eq '{buildingProfessionApplicationModelNewApplication.PersonalDetails.ApplicantName.LastName.EscapeSingleQuote()}' and statuscode eq 1 and emailaddress1 eq '{buildingProfessionApplicationModelNewApplication.PersonalDetails.ApplicantEmail.Email.EscapeSingleQuote()}' and contains(telephone1, '{buildingProfessionApplicationModelNewApplication.PersonalDetails.ApplicantPhone.PhoneNumber.Replace("+", string.Empty).EscapeSingleQuote()}')")
+            .WithQueryParam("$expand", "bsr_contacttype_contact")
+            .WithVerb(HttpMethod.Get);
+
+            Assert.NotNull(testGetContacts);
+            Assert.Equal(dynamicsContact.contactid, testGetContacts.contactid);
+
+        }
+
+        [Fact]
         public async Task CreateDynamicsPaymentAsync()
         {
 
