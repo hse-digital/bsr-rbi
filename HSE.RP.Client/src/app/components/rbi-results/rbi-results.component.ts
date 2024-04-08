612a8a8ea11100934ec1b10a790cbb62371b16d1
Sean Lynch Codec Systems <slynch@codec.ie>
2024-04-08 09:35:55 +0000

    Merged PR 2220: Fix pagination rendering bugs and return to results
    Related work items: #23932, #23933, #23965

--- HSE.RP.Client/src/app/components/rbi-results/rbi-results.component.ts
+++ HSE.RP.Client/src/app/components/rbi-results/rbi-results.component.ts
@@ -104,6 +104,9 @@ export class RBIResultsComponent implements OnInit {
 
     this.getApplicationPage();
 
+    this.page = 1;
+    this.currentPage = 1;
+
   }
 
   async startSearch() {
@@ -235,7 +238,6 @@ export class RBIResultsComponent implements OnInit {
 
   }
 
-  //Function to get an array containing the first, last and the current page plus the items one before and after the current page, include the first and last page in the array at the first and last indiex
   getPages() {
     let pages = [this.currentPage];
     let i = 1;
